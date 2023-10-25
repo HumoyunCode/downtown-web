2f8dca947303008b0f51a9906f6ab223cd8678fa
apple <bahrom1982@gmail.com>
2023-10-25 11:29:56 +0500

    finish regsistration form

--- js/main.js
+++ js/main.js
@@ -3,12 +3,16 @@ document.addEventListener('DOMContentLoaded', function () {
     const sidebar = document.querySelector(".sidebar-1");
     const sidebarRegistration = document.querySelector(".sidebar-2");
     const menuBtn = document.querySelector(".menu");
+    const registrationSidebar = document.querySelector(".main-sidebar");
 
     function toggleSidebar() {
         sidebar.classList.toggle('close_toggle')
         sidebarRegistration.classList.toggle('close_toggle')
+        registrationSidebar.classList.toggle('close_toggle')
     }
 
     menuBtn.addEventListener('click', toggleSidebar);
+
+    // registration form side bar function
 });
 

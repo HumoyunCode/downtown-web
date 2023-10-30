2c575369574487aba9a30cf22652ba863a4f8117
Humoyun <greatman19990810@gmail.com>
2023-10-30 19:45:55 +0500

    finished move btn

--- js/main.js
+++ js/main.js
@@ -28,48 +28,102 @@ document.addEventListener('DOMContentLoaded', function () {
       progressBar.style.width = width + "%";
     }
     // Example: Update the progress bar when a test is completed
-    updateProgressBar(1); // Call this function with the number of completed tests
-    const bigDiv = document.querySelector(".card");
+    updateProgressBar(3); // Call this function with the number of completed tests
+
+    // listening button javascript code
     const moveButton = document.querySelector("#btn_listening");
-    // const moveButton1 = document.querySelector("");
-    const testArea = document.querySelector(".test_area");
+    const testAreaListening = document.querySelector(".listening");
     const imageListeningSources = ['/image/listeningClick.jpg', '/image/listening.jpg'];
-    const imageReadingSources = ['/image/readingClick.jpg', '/image/reading.jpg'];
-    const imageWritingSources = ['/image/writingClick.jpg', '/image/writinging.jpg'];
-    const imageSpeakingSources = ['/image/speakingClick.jpg', '/image/speaking.jpg'];
+    const imageReadingSources = ['/image/readingClick.jpg', '/image/reading.png'];
+    const imageWritingSources = ['/image/writingClick.jpg', '/image/writing.png'];
     const buttonImage = document.getElementById('buttonImage');
-    
-    let currentPosition = 0;
 
     // Variable to track if the div is already moved
     let isMoved = false;
 
     moveButton.addEventListener("click", () => {
       if (!isMoved) {
-        // Move the big div to the left slowly
-
-        // Change the display property of the test area to 'flex'
-        // bigDiv.style.justifyContent = "space-between"
-        // testArea.style.display = "flex";
-        testArea.style.opacity = "1";
-        testArea.style.transition = "1s ease";
+        testAreaListening.style.opacity = "1";
+        testAreaListening.style.transition = "1s ease";
         // // bunga transform bilan birga qilinadi opasitiy bilan qilsa ham bo`ladi 
         moveButton.classList.add('btn_clicked');
          buttonImage.src = imageListeningSources[0];
       } else {
-        // Reset the big div position and hide the test area
-        // moveButton.style.transition = "margin-left 20s";
-        // moveButton.style.marginLeft = "auto";
-        // // testArea.style.display = "none";
-        testArea.style.opacity = "0";
-        // // bigDiv.style.justifyContent = "center"
+        testAreaListening.style.opacity = "0";
         moveButton.classList.remove('btn_clicked')
-        testArea.style.transition = ".5s";
+        testAreaListening.style.transition = ".5s";
         buttonImage.src = imageListeningSources[1];
       }
 
       isMoved = !isMoved;
     });
 
+    // reading button javascript code
+
+    const moveButtonReading = document.querySelector("#btn_reading");
+    const testAreaReading = document.querySelector(".reading");
+    const buttonImageReading = document.getElementById('buttonImageReading');
+    let movedReading = false;
+    moveButtonReading.addEventListener("click", () => {
+      if (!movedReading) {
+        testAreaReading.style.opacity = "1";
+        testAreaReading.style.transition = "1s ease";
+        // // bunga transform bilan birga qilinadi opasitiy bilan qilsa ham bo`ladi 
+        moveButtonReading.classList.add('btn_clicked');
+         buttonImageReading.src = imageReadingSources[0];
+      } else {
+        testAreaReading.style.opacity = "0";
+        moveButtonReading.classList.remove('btn_clicked')
+        testAreaReading.style.transition = ".5s";
+        buttonImageReading.src = imageReadingSources[1];
+      }
+
+      movedReading = !movedReading;
+    });
+
+    // writing button javascript code
+    const moveButtonWriting = document.querySelector("#btn_writing");
+    const testAreaWriting = document.querySelector(".writing");
+    const buttonImageWriting = document.getElementById('buttonImageWriting');
+    let movedWriting = false;
+    moveButtonWriting.addEventListener("click", () => {
+      if (!movedWriting) {
+        testAreaWriting.style.opacity = "1";
+        testAreaWriting.style.transition = "1s ease";
+        // // bunga transform bilan birga qilinadi opasitiy bilan qilsa ham bo`ladi 
+        moveButtonWriting.classList.add('btn_clicked');
+        buttonImageWriting.src = imageWritingSources[0];
+      } else {
+        testAreaWriting.style.opacity = "0";
+        moveButtonWriting.classList.remove('btn_clicked')
+        testAreaWriting.style.transition = ".5s";
+        buttonImageWriting.src = imageWritingSources[1];
+      }
+
+      movedWriting = !movedWriting;
+    });
+
+    // speaking button javascript code
+    const moveButtonSpeaking = document.querySelector("#btn_speaking");
+    const testAreaSpeaking = document.querySelector(".speaking");
+    const buttonImageSpeaking = document.getElementById('buttonImageSpeaking');
+    const imageSpeakingSources = ['/image/speakingClick.jpg', '/image/speaking.png'];
+    let movedSpeaking = false;
+    moveButtonSpeaking.addEventListener("click", () => {
+      if (!movedSpeaking) {
+        testAreaSpeaking.style.opacity = "1";
+        testAreaSpeaking.style.transition = "1s ease";
+        // // bunga transform bilan birga qilinadi opasitiy bilan qilsa ham bo`ladi 
+        moveButtonSpeaking.classList.add('btn_clicked');
+        buttonImageSpeaking.src = imageSpeakingSources[0];
+      } else {
+        testAreaSpeaking.style.opacity = "0";
+        moveButtonSpeaking.classList.remove('btn_clicked')
+        testAreaSpeaking.style.transition = ".5s";
+        buttonImageSpeaking.src = imageSpeakingSources[1];
+      }
+
+      movedSpeaking = !movedSpeaking;
+    });
 });
 
